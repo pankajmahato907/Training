ac1a043f712c1b1fa398bf80a37f8a71b8779e90
pankaj <pankumahato123@gmail.com>
2025-07-28 10:11:29 +0545

    JS statrted...learnn the console types, varibales, data types ,operators

---
+++ JS/Day11/main.js
@@ -0,0 +1,85 @@
+
+//Console types
+//console.log("Hello World"); ///console printout garxa results
+//console.error("Hello");
+// console.warn("Hello");   //warning through
+
+// window.alert("Welcome to page")
+
+
+//variable
+
+// var a=10;
+// var a=20;
+// console.log(a)
+
+//Global Scope
+// {
+//     var a= 10;
+// }
+// console.log(a);
+
+
+/* Block Scope
+let
+{
+    let a=10;
+    // console.log(a)
+}
+console.log(a)//this console wil through error due to block scope */
+
+
+//const  (Value change garna mildaina)(redcleare ni gardaina)
+/* const a=20;
+console.log(a)
+
+/* 
+--Data Types
+ 1)Primitive data types
+number, string, boolean, null, undefined
+ 2)non primitive data types
+ object, array, function */
+
+ /* //number
+ let a=10;
+ console.log(typeof a); */
+
+
+ //string
+ /*
+ var name=("Pankaj")
+console.log(typeof name); */
+
+//booleean
+/* let isholiday= true;
+console.log(typeof isholiday) */
+
+
+//null: data rakhne thau xa but data xaina
+/* let user=null;
+console.log(typeof user); */
+
+
+//undefined
+/* let a;
+console.log(a); //variable declare grya xam but value xaina so it will through undefined */
+
+
+//Operators types
+
+/* 1)Arithmetic
+2)Assignment
+3)Comparisoon (= means assign matra garne, == means vlaue check, === value check + data types ni herxa)
+example of ===: a=1;
+                b="1";
+        
+4)Ternary : if else conditon
+           symbol used= : means false , ? means true conditon
+5)Logical
+         symbol: && , || = auta conditon ni true vaye true hune , ! = not true 
+*/
+
+
+
+
+
---
+++ JS/Day12/Operators/main.js
@@ -0,0 +1,84 @@
+// Arithemetic operators
+//addition
+/* let a= 5+4
+console.log(a); */
+
+//Division
+/* let a= 9/3;
+console.log(a); */
+
+//Modulus or remainder
+/* let a=10%3
+console.log(a); */
+
+//Exponential
+/* let a= 2**3  //2*2*2
+console.log(a) */
+
+
+//Increment
+/* let a=3
+a++;  //a+1 i.e. 3+1=4
+console.log(a); */
+
+
+//Decrement
+
+//........Assignment Operators
+// = Assign
+// ==
+/* let a=1
+let b="1"
+console.log(a==b); //this show true answer it only checks value not data so 
+console.log(a===b); // this will show false beacuse it also check datatypes + value */
+
+
+// +=
+/* let a=3;
+a +=1; //a+3=4
+console.log(a);  */
+
+
+
+//...................Comparision Operator............................//
+//=
+//==
+//===
+
+// != operator
+
+/* let a=4;
+let b= "5";
+console.log(a != b); //it thorugh false beacuse it checks value also and data types also ..
+console.log( a !==b); //it will through true  beacuse it will check only value */
+
+
+//<=
+//>=
+
+
+//..................................Logical Operator.....................
+
+//&& operator :: dui tai value true hunu parne 
+
+/* let a=true;
+let b= false;
+
+console.log(a && b)
+ */
+
+// || operator //auta value ni true vayo vane true
+
+/* let c=true;
+let d= false;
+
+console.log(a || b) //ouptut: true */
+
+
+// ! operator
+
+/* let a= true;
+console.log(!a); */
+
+
+
